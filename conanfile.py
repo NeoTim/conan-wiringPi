7f6a550cb3846cefc55a76593e73611d8cfa9a92
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-07-18 23:02:35 +0200

    Add Conan Package File

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, tools
+
+
+class WiringpiConan(ConanFile):
+    name = "wiringPi"
+    version = "2.46"
+    license = "GNU LGPLv3"
+    url = "https://git.drogon.net/?p=wiringPi;a=summary"
+    author="Gordon Henderson <projects@drogon.net>"
+    description = "WiringPi is a PIN based GPIO access library written in C. (for Raspberry Pi)"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [False]}
+    default_options = "shared=False"
+    generators = "compiler_args"
+
+    def source(self):
+        self.run("git clone git://git.drogon.net/wiringPi")
+        self.run("cd wiringPi")
+
+
+    def build(self):
+        ### edit Makefile
+        ## create Installation Dir
+        self.run("mkdir -p %s/_install-tmp/include" % self.source_folder)
+        self.run("mkdir -p %s/_install-tmp/lib" % self.source_folder)
+        self.run("mkdir -p %s/_install-tmp/bin" % self.source_folder)
+        self.run("mkdir -p %s/_install-tmp/src" % self.source_folder)
+
+        ## change Destination Dir
+        self.run("sed -i 's+PREFIX?=/local+PREFIX?=+g' %s" % (self.source_folder + "/wiringPi/wiringPi/Makefile"))
+        self.run("sed -i 's+DESTDIR?=/usr+DESTDIR?=%s/_install-tmp+g' %s" % ((self.source_folder), (self.source_folder + "/wiringPi/wiringPi/Makefile")))
+
+	## build wiringPi
+        self.run("cd %s/wiringPi/wiringPi && make -j5 && make install" % self.source_folder)
+	
+    def package(self):
+        self.copy("*.h", dst="include", src=self.source_folder+"/_install-tmp/include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["wiringPi"]
